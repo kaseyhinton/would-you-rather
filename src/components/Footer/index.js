63a121a1035087dee6ae216a26d954f47dd697aa
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-05 13:07:44 -0600

    Adds footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './index.css';
+
+export default function () {
+	return (
+		<footer className={ style.footer }>
+			<span><a href="https://github.com/kaseyhinton">Kasey Hinton</a></span>
+		</footer>
+	);
+}
