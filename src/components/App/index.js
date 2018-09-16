e5af6b2574f7018ab87e60ca80280dec74b173d5
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-15 18:05:25 -0600

    Adds subscription to store and formats code.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import Loadable from 'react-loadable';
-import { HMR } from '@pwa/preset-react';
-import { Route, withRouter } from 'react-router-dom';
-import Footer from '@components/Footer';
-import style from './index.css';
-import Nav from '../Nav';
+import React from "react";
+import Loadable from "react-loadable";
+import { HMR } from "@pwa/preset-react";
+import { Route, withRouter } from "react-router-dom";
+import Footer from "@components/Footer";
+import style from "./index.css";
+import Nav from "../Nav";
+
+import {store} from "../../store";
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
 // Route-Split Components
 const loading = () => <div>Loading...</div>;
 const load = loader => Loadable({ loader, loading });
 
-const Home = load(() => import('@pages/Home'));
-const Profile = load(() => import('@pages/Profile'));
-const LeaderBoard = load(() => import('@pages/LeaderBoard'));
-const Login = load(() => import('@pages/Login'));
+const Home = load(() => import("@pages/Home"));
+const Profile = load(() => import("@pages/Profile"));
+const LeaderBoard = load(() => import("@pages/LeaderBoard"));
+const Login = load(() => import("@pages/Login"));
 
 class App extends React.Component {
-	componentDidMount() {
-		if (process.env.NODE_ENV === 'production') {
-			this.props.history.listen(obj => {
-				if (window.ga) ga.send('pageview', { dp:obj.pathname });
-			});
-		}
-	}
+  componentDidMount() {
+    if (process.env.NODE_ENV === "production") {
+      this.props.history.listen(obj => {
+        if (window.ga) ga.send("pageview", { dp: obj.pathname });
+      });
+    }
+  }
 
-	render() {
-		return (
-			<div className={ style.app }>
-			<Nav />
-				<main className={ style.wrapper }>
-					<Route path="/" exact component={ Home } />
-					<Route path="/leaderboard" exact component={ LeaderBoard } />
-					<Route path="/profile" exact component={ Profile } />
-					<Route path="/login" exact component={ Login } />
-					{/* <Route path="/blog/:title" component={ Article } /> */}
-				</main>
+  render() {
+    return (
+      <div className={style.app}>
+        <Nav />
+        <main className={style.wrapper}>
+          <Route path="/" exact component={Home} />
+          <Route path="/leaderboard" exact component={LeaderBoard} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/login" exact component={Login} />
+          {/* <Route path="/blog/:title" component={ Article } /> */}
+        </main>
 
-				<Footer />
-			</div>
-		);
-	}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default HMR(withRouter(App), module);
