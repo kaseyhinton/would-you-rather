fd419d2d62ef702aca4498a5404758444c61be57
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-17 13:05:56 -0600

    Adds add question to redux. fixes some typos

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,13 +1,30 @@
-import { _getQuestions } from "../../_DATA";
+import { _getQuestions, _saveQuestion } from "../../_DATA";
 import { apologize } from "./app";
 
 export const GET_QUESTIONS = "GET_QUESTIONS";
 export const SET_QUESTIONS = "SET_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
+
+export const addQuestionAsync = question => {
+  return dispatch => {
+    return _addQuestion().then(
+      question => dispatch(_saveQuestion(question)),
+      error => dispatch(apologize(error))
+    );
+  };
+}
+
+export const addQuestion = question => {
+  return {
+    type: ADD_QUESTION,
+    question
+  }
+}
 
 export const getQuestions = () => {
   return dispatch => {
     return _getQuestions().then(
-      users => dispatch(setQuestions(users)),
+      questions => dispatch(setQuestions(questions)),
       error => dispatch(apologize(error))
     );
   };
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,12 +1,20 @@
-import { SET_QUESTIONS } from "../actions/questions";
+import { SET_QUESTIONS, ADD_QUESTION } from "../actions/questions";
 
 const questions = (state = {}, action) => {
   switch (action.type) {
-    case SET_USERS:
+    case SET_QUESTIONS:
       return {
         ...state,
         questions: action.questions
       };
+    case ADD_QUESTION:
+      return {
+        ...state,
+        questions: {
+          ...state.questions,
+          question: action.question
+        }
+      }
     default:
       return state;
   }
