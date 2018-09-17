38eb737fc330502f27da5093ea52e79a23f7f5d7
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-17 13:37:10 -0600

    Adds AddQuestion Component

---
+++ src/pages/AddQuestion/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { HMR } from "@pwa/preset-react";
+import style from "./index.css";
+
+import "@material/button/dist/mdc.button.css";
+import "@material/card/dist/mdc.card.css";
+import "@material/floating-label/dist/mdc.floating-label.css";
+import "@material/notched-outline/dist/mdc.notched-outline.css";
+import "@material/line-ripple/dist/mdc.line-ripple.css";
+import '@material/notched-outline/dist/mdc.notched-outline.css';
+import '@material/textfield/dist/mdc.textfield.css';
+
+import { TextField } from "@rmwc/textfield";
+import { Button } from "@rmwc/button";
+import { Card, CardActions } from "@rmwc/card";
+import { Typography } from "@rmwc/typography";
+import { connect } from "react-redux";
+
+import { addQuestionAsync } from "../../actions/questions";
+import { store } from "../../store";
+
+class AddQuestion extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      optionOne: "",
+      optionTwo: ""
+    };
+  }
+
+  handleOptionOne = event => {
+    this.setState({optionOne: event.target.value});
+  };
+
+  handleOptionTwo = event => {
+    this.setState({optionTwo: event.target.value});
+  };
+
+  handleSaveQuestion = () => {
+    const question = {
+      author: this.props.userId,
+      optionOneText: this.state.optionOne,
+      optionTwoText: this.state.optionTwo
+    }
+    store.dispatch(addQuestionAsync(question));
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className={style.login}>
+        <Card
+          style={{
+            width: "100%",
+            padding: "1rem"
+          }}
+        >
+          <Typography className={style.header} use="headline2" tag="h2">
+            Add Question
+          </Typography>
+          <Typography className={style.header} use="headline4" tag="h4">
+          Would you rather?
+          </Typography>
+          <TextField onChange={this.handleOptionOne.bind(this)} outlined />
+          <Typography className={style.header} use="headline4" tag="h4">
+          Or
+          </Typography>
+          <TextField onChange={this.handleOptionTwo.bind(this)} outlined />
+
+          <CardActions
+            style={{
+              flexDirection: "column",
+              alignItems: "flex-end",
+              padding: "0 1rem"
+            }}
+          >
+            <Button onClick={this.handleSaveQuestion.bind(this)} disabled={!this.state.optionOne || !this.state.optionTwo} outlined>
+              SAVE
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    userId: state.app.user
+  };
+};
+
+export default HMR(connect(mapStateToProps)(AddQuestion), module);
