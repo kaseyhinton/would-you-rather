aff34908f1263134eb7592febf2404631fe6800a
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-17 13:37:01 -0600

    Adds getUsers and getQuestions on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { HMR } from '@pwa/preset-react';
 import style from './index.css';
+import { getQuestions } from "../../actions/questions";
+import { getUsers } from "../../actions/users";
+import { store } from '../../store';
 
-function Home() {
-	return (
+class Home extends React.Component {
+
+	componentWillMount() {
+		store.dispatch(getQuestions());
+		store.dispatch(getUsers());
+	}
+
+	render() {
+		return (
 		<div className={ style.home }>
 			<div className={ style.titles }>
 				<h1>Would You Rather?</h1>
@@ -11,6 +21,7 @@ function Home() {
 			</div>
 		</div>
 	);
+	}
 }
 
 export default HMR(Home, module);
