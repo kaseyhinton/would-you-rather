950722e7f6f74bcf93ee9b5f2411880184613d0c
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-19 23:19:20 -0600

    Adds save answer action in questions that dispatched questions and users update after successfully saving.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,23 @@
-import { _getQuestions, _saveQuestion } from "../../_DATA";
+import { _getQuestions, _saveQuestion, _saveQuestionAnswer } from "../../_DATA";
 import { apologize } from "./app";
+import { getUsers } from "./users";
 
 export const GET_QUESTIONS = "GET_QUESTIONS";
 export const SET_QUESTIONS = "SET_QUESTIONS";
 export const ADD_QUESTION = "ADD_QUESTION";
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
+
+export const answerQuestionAsync = answerDto => {
+  return dispatch => {
+    return _saveQuestionAnswer(answerDto).then(
+      result => {
+        dispatch(getQuestions());
+        dispatch(getUsers());
+      },
+      error => dispatch(apologize(error))
+    );
+  };
+};
 
 export const addQuestionAsync = question => {
   return dispatch => {
@@ -12,14 +26,14 @@ export const addQuestionAsync = question => {
       error => dispatch(apologize(error))
     );
   };
-}
+};
 
 export const addQuestion = question => {
   return {
     type: ADD_QUESTION,
     question
-  }
-}
+  };
+};
 
 export const getQuestions = () => {
   return dispatch => {
